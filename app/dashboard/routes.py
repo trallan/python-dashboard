7cfc4c858a7d23f5ffeb7d40298a73e85f705935
Viktor Erlandsson <72042504+trallan@users.noreply.github.com>
2026-05-01 16:24:31 +0000

    Add - read data

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,12 +1,14 @@
 import json
+from pathlib import Path
 from . import dashboard_bp
-
 from flask import render_template
+
+base_dir = Path(__file__).parent
+db_path = base_dir / "../db/data.json"
 
 @dashboard_bp.route("/")
 def index():
-    with open("../db/data.json") as f:
+    with open(db_path) as f:
         data = json.load(f)
-        headers = data.keys(f)
 
-    return render_template("dashboard/index.html", header=headers, data=data)
+    return render_template("dashboard/index.html", headers=data[0].keys(), data=data)
